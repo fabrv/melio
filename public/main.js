7d077f4d012da5ab7174ab2e077d3e5ff7951a43
fabrv <frivera@nevicad.com>
2021-12-26 15:14:19 -0600

    submit modal

--- public/main.js
+++ public/main.js
@@ -24,29 +24,61 @@ const addClass = (elementId, className) => {
   element.classList.add(className);
 }
 
+let rating = 0;
+
 const stars = document.querySelectorAll('.select-star');
-  const starMouseEnter = (e) => {
-    const id = e.target.id.replace('star', '');
+const starMouseEnter = (e) => {
+  const id = e.target.id.split('star')[1];
 
-    for (let i = 0; i < 5; i++) {
-      if (i <= id) {
-        stars[i].classList.add('bi-star-fill');
-        stars[i].classList.remove('bi-star');
-      } else {
-        stars[i].classList.remove('bi-star-fill');
-        stars[i].classList.add('bi-star');
-      }
-    }
-  }
-
-  const starMouseLeave = (e) => {
-    for (let i = 0; i < 5; i++) {
-      stars[i].classList.remove('bi-star-fill');
+  for (let i = 0; i < stars.length; i++) {
+    const starId = stars[i].id.split('star')[1];
+    if (starId <= id) {
+      stars[i].classList.add('bi-star-fill');
+      stars[i].classList.remove('bi-star');
+    } else {
       stars[i].classList.add('bi-star');
+      stars[i].classList.remove('bi-star-fill');
     }
   }
+}
 
-  stars.forEach((star) => {
-    star.addEventListener('mouseenter', starMouseEnter);
-    star.addEventListener('mouseleave', starMouseLeave);
-  });
+const starMouseLeave = () => {
+  for (let i = 0; i < 5; i++) {
+    stars[i].classList.remove('bi-star-fill');
+    stars[i].classList.add('bi-star');
+  }
+}
+
+const starClick = (e) => {
+  rating = parseInt(e.target.id.split('star')[1]) + 1;
+  displayRating(rating);
+}
+
+stars.forEach((star) => {
+  star.addEventListener('mouseenter', starMouseEnter);
+  star.addEventListener('mouseleave', starMouseLeave);
+  star.addEventListener('click', starClick);
+});
+
+const displayRating = (rating) => {
+  const modalStars = document.querySelectorAll('.modal-body .select-star');
+  for (let i = 0; i < 5; i++) {
+    if (i < rating) {
+      modalStars[i].classList.add('bi-star-fill');
+      modalStars[i].classList.remove('bi-star');
+    } else {
+      modalStars[i].classList.add('bi-star');
+      modalStars[i].classList.remove('bi-star-fill');
+    }
+  }
+}
+
+var rateModal = document.getElementById('rateModal')
+rateModal.addEventListener('show.bs.modal', function (event) {
+  var button = event.relatedTarget
+  var modalRating = button.getAttribute('data-rating')
+  var modalTitle = rateModal.querySelector('.modal-title')
+
+  displayRating(parseInt(modalRating) + 1);
+})
+
